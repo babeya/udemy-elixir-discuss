b8546c2df76068287736a63e3d92b8fefe0286d1
Arthur BABEY <ababey@greenflex.com>
2019-07-14 12:22:53 +0200

    Basic navigation

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,19 +1,27 @@
 defmodule Discuss.TopicController do
     use Discuss.Web, :controller
-  
+    
     alias Discuss.Topic
-
+    
+    def index(conn, _params) do
+        topics = Repo.all(Topic)
+        render conn, "index.html", topics: topics
+    end
+    
     def new(conn, _params) do
         changeset = Topic.changeset(%Topic{}, %{})
         render conn, "new.html", changeset: changeset
     end
-
-    def create(_conn, %{"topic" => topic}) do         
+    
+    def create(conn, %{"topic" => topic}) do         
         changeset = Topic.changeset(%Topic{}, topic)
         case Repo.insert(changeset) do
-            {:ok, post } -> IO.inspect(post)
-            {:error, changeset} -> IO.inspect(changeset)
+            {:ok, post } -> 
+                conn 
+                |> put_flash(:info,"Topic Created")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> 
+                render conn, "new.html", changeset: changeset
         end
     end
 end
-  
